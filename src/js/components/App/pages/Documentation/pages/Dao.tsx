4e3ee907b518e8336ed563f23e3580eadfe18d33
veeso <christian.visintin@veeso.dev>
2024-12-16 13:12:10 +0100

    fix: dao

---
+++ src/js/components/App/pages/Documentation/pages/Dao.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import MdPage from '../MdPage';
+
+const Dao = () => (
+  <MdPage
+    url={
+      'https://raw.githubusercontent.com/EKOKEtoken/ekoke/refs/heads/main/docs/dao.md'
+    }
+    githubUrl="https://github.com/EKOKEtoken/ekoke/blob/main/docs/dao.md"
+  />
+);
+
+export default Dao;
